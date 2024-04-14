f0a829886f72dda83e68851b47398275e2a5a0c1
thiago.silva <thiagosilva327@live.com>
2024-04-14 13:22:00 -0300

    feat: install and use bootstrap css in webpack

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }} - Controle de Séries</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}/css/app.css">
 </head>
 
 <body>
