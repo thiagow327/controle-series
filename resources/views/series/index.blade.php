877b629b2b30b708ef5d5f9d1e3520e5a7d51839
thiago.silva <thiagosilva327@live.com>
2024-04-14 12:55:53 -0300

    feat: using function 'htmlentities()' para evitar Cross-site scripting (XSS)

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,9 +1,10 @@
 <x-layout title="Séries">
     <a href="/series/criar">Adicionar</a>
-    
+
     <ul>
         @foreach ($series as $serie)
-            <li> {{ $serie }} </li>
+            <li> {{ htmlentities($serie) }} </li>
+            <!-- usamos 'htmlentities' para escapes evitando Cross-site scripting (XSS) -->
         @endforeach
     </ul>
 </x-layout>
