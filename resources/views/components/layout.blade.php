aea1e466e8fd925a5b04129d1dc34cb5ad194f24
thiago.silva <thiagosilva327@live.com>
2024-04-14 11:14:23 -0300

    feat: create layouts in blade

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,17 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Series</title>
+    <title>{{ $title }} - Controle de Séries</title>
 </head>
 
 <body>
-    <h1>Series</h1>
+    <h1>{{ $title }}</h1>
 
-    <ul>
-        @foreach ($series as $serie)
-            <li> {{ $serie }} </li>
-        @endforeach
-    </ul>
+    {{ $slot }}
 </body>
 
 </html>
