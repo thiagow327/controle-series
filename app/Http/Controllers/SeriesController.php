35af5507cd08dc6c1c7d8b2f65aa6e645de06e13
thiago.silva <thiagosilva327@live.com>
2024-04-15 18:36:38 -0300

    feat: session and flash messages

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Serie;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $series = Serie::query()->orderBy('nome')->get();
+        $mensagemSucesso = session('mensagem.sucesso');
 
-        return view('series.index')->with('series', $series); // with get argument string and create array with key : variable
+        return view('series.index')->with('series', $series)
+            ->with('mensagemSucesso', $mensagemSucesso); // with get argument string and create array with key : variable
     }
 
     public function create()
@@ -36,12 +39,14 @@ class SeriesController extends Controller
         Serie::create([
             'nome' => $request->input('nome'),
         ]);
+        $request->session()->flash('mensagem.sucesso', 'Série adicionada com sucesso');
         return to_route('series.index');
     }
 
     public function destroy(Request $request)
     {
         Serie::destroy($request->series);
+        $request->session()->flash('mensagem.sucesso', 'Série removida com sucesso');
 
         return to_route('series.index');
     }
